d12c4ee673e5f13130b249edda58e0bbde1c94f9
Abdul Mu'min <abdmukmin@rocketmail.com>
2018-07-31 10:48:44 +0700

    Upload exercises 4 week 2

---
+++ Exercises 4.js
@@ -0,0 +1,44 @@
+var tanggal = 1 
+var bulan = 9
+var tahun = 1990
+var sebutBulan = ''
+
+switch (bulan){
+	case 1:
+	  sebutBulan = 'Januari';
+	  break;
+	case 2:
+	  sebutBulan = 'Februari';
+	  break;
+	case 3:
+	  sebutBulan = 'Maret';
+	  break;
+	case 4:
+	  sebutBulan ='April';
+	  break;
+	case 5:
+	  sebutBulan = 'May';
+	  break;
+	case 6:
+	  sebutBulan ='Juni';
+	  break;
+	case 7:
+	  sebutBulan ='Juli';
+	  break;
+	case 8:
+	  sebutBulan = 'Agustus';
+	  break;
+	case 9:
+	  sebutBulan = 'September'
+	  break;
+	case 10:
+	  sebutBulan = 'Oktober';
+	  break;
+	case 11:
+	  sebutBulan = 'November';
+	  break;
+	case 12:
+	  sebutBulan = 'Desember';
+	  break;
+}
+console.log(''+ tanggal +' '+sebutBulan+' '+tahun+'')
