c3a601091b392467334672563de53971ee988264
Abdul Mu'min <abdmukmin@rocketmail.com>
2018-08-02 22:08:02 +0700

    Pembatasan parameter input tanggal bulan dan tahun

--- Exercises 4.js
+++ Exercises 4.js
@@ -1,44 +1,74 @@
-var tanggal = 1 
-var bulan = 9
-var tahun = 1990
+var tanggal = 5
+var bulan = 7
+var tahun = 2001
 var sebutBulan = ''
 
-switch (bulan){
-	case 1:
-	  sebutBulan = 'Januari';
-	  break;
-	case 2:
-	  sebutBulan = 'Februari';
-	  break;
-	case 3:
-	  sebutBulan = 'Maret';
-	  break;
-	case 4:
-	  sebutBulan ='April';
-	  break;
-	case 5:
-	  sebutBulan = 'May';
-	  break;
-	case 6:
-	  sebutBulan ='Juni';
-	  break;
-	case 7:
-	  sebutBulan ='Juli';
-	  break;
-	case 8:
-	  sebutBulan = 'Agustus';
-	  break;
-	case 9:
-	  sebutBulan = 'September'
-	  break;
-	case 10:
-	  sebutBulan = 'Oktober';
-	  break;
-	case 11:
-	  sebutBulan = 'November';
-	  break;
-	case 12:
-	  sebutBulan = 'Desember';
+//cek tanggal 
+switch (true){
+	case tanggal >= 1 && tanggal <= 31 :    
+
+    //cek bulan
+    switch (true){
+      case bulan >= 1 && bulan <=12 :
+
+        //cek tahun
+        switch (true){
+          case tahun >=1900 && tahun <= 2200 :
+
+			//kalo sudah benar semua baruu proses konversi bulan
+            switch (bulan){
+              case 1:
+                sebutBulan = 'Januari';
+                break;
+              case 2:
+                sebutBulan = 'Februari';
+                break;
+              case 3:
+                sebutBulan = 'Maret';
+                break;
+              case 4:
+                sebutBulan ='April';
+                break;
+              case 5:
+                sebutBulan = 'May';
+                break;
+              case 6:
+                sebutBulan ='Juni';
+                break;
+              case 7:
+                sebutBulan ='Juli';
+                break;
+              case 8:
+                sebutBulan = 'Agustus';
+                break;
+              case 9:
+                sebutBulan = 'September'
+                break;
+              case 10:
+                sebutBulan = 'Oktober';
+                break;
+              case 11:
+                sebutBulan = 'November';
+                break;
+              case 12:
+                sebutBulan = 'Desember';
+                break;
+			//baru tampilkan semuanya
+            } console.log(''+ tanggal +' '+sebutBulan+' '+tahun+'')
+
+          break
+          default :
+            console.log('inputan tahun keliru')
+            break 
+        }
+        break
+      default :
+        console.log('inputan bulan keliru')
+        break;
+    }
+
+    break
+	default :
+	  console.log('inputan tanggal keliru');
 	  break;
 }
-console.log(''+ tanggal +' '+sebutBulan+' '+tahun+'')
