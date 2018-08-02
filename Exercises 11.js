e230997b47db624268d24b28e61362bfd311d7c9
Abdul Mu'min <abdmukmin@rocketmail.com>
2018-08-02 15:23:32 +0700

    Upload exercises 11.js

--- Exercises 11.js
+++ Exercises 11.js
@@ -1,20 +1,13 @@
-var jam = 0
-var menit = 0
+//---------------------------------------------------------
+//Logic Challenge - Balik Kata-----------------------------
 
-function konversiMenit(menit){
-    while (menit >= 60){
-      menit -= 60
-      jam = jam + 1
-      if (menit <11){
-        menit = '0'+menit
-        }      
-    }return (jam+':'+menit)
+function balikKata(kata) {
+  return kata.split('').reverse().join('');
 }
 
-
 // TEST CASES
-console.log(konversiMenit(63)); // 1:03
-console.log(konversiMenit(124)); // 2:04
-console.log(konversiMenit(53)); // 0:53
-console.log(konversiMenit(88)); // 1:28
-console.log(konversiMenit(120)); // 2:00
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
