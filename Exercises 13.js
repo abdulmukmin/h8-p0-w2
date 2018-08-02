49a066bc1ed25f5ff1e39a0cbf7dc49d8a8440cc
Abdul Mu'min <abdmukmin@rocketmail.com>
2018-08-02 14:58:26 +0700

    Upload exercises 13 week 2

---
+++ Exercises 13.js
@@ -0,0 +1,29 @@
+//---------------------------------------------
+//Logic Challenge - X dan O--------------------
+
+var str = ''
+var x = 0
+var o = 0
+
+function xo(str){
+  for(i = 0; i <= str.length ; i++){
+    str.charAt(i)
+    if (str.charAt(i)==='x'){
+      x =  x + 1
+    } else if(str.charAt(i)==='o'){
+      o = o + 1
+    }
+  }
+
+  if (x === o){
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(xo('xoxoxo')); // true
+//console.log(xo('oxooxo')); // false
+//console.log(xo('oxo')); // false
+//console.log(xo('xxxooo')); // true
+//console.log(xo('xoxooxxo')); // true
